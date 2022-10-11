d84b7f64aef8fbfa35960ef0d2375aa860c49e37
rathipinniyogiswar <91770368+rathipinniyogiswar@users.noreply.github.com>
2022-10-11 11:12:11 +0530

    yogiswar

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
+length_of_circular_linked_list = int(input())
+circular_linked_list = list(map(int,input().strip().split(" ")))
+actual_list = []
 
+value = 0
+while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
+    element = circular_linked_list[value]
+    if element not in actual_list:
+        actual_list.append(element)
+    value += 1
+
+print(len(actual_list))
+print(" ".join(str(num) for num in actual_list))
